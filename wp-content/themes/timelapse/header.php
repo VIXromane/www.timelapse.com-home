97611463e00ff3d0932536072d4c8914d3e066c8
Romane Vix <vix.romane@gmail.com>
2016-05-31 16:06:29 +0200

    début du header

--- wp-content/themes/timelapse/header.php
+++ wp-content/themes/timelapse/header.php
@@ -32,34 +32,35 @@ session_start();
 
 	<?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 	<?php if( (is_front_page() || is_home()) && (!isset($_SESSION['first'])) ) : 
 	
 
-		$_SESSION['first'] = 'ok';
+	$_SESSION['first'] = 'ok';
 
 	?>
-<section class="adieu" id="intro">
-	
+	<section class="adieu" id="intro">
 
 
-	<div id="visu-intro">
-		<div id="circles">
-			<div id="point1" class="points"></div>
-			<div id="point2" class="points"></div>
-			<div id="point3" class="points"></div>
-			<div id="point4" class="points"></div>
 
+		<div id="visu-intro">
+			<div id="circles">
+				<div id="point1" class="points"></div>
+				<div id="point2" class="points"></div>
+				<div id="point3" class="points"></div>
+				<div id="point4" class="points"></div>
+
+			</div>
 		</div>
-	</div>
 
-	<div class="intro-content text-center">
-		<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/_visuels/complete-logo-white-timelapse.png" alt="logo full verison Timelapse introduction">
-		<h1>Montres intemporelles de collection</h1>
-		<a class="removeme" href="#" id="action">Trouvez votre modèle<br/>
-			<i class="fa fa-angle-down" aria-hidden="true"></i>
-		</a>
-	</div>
+		<div class="intro-content text-center">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/_visuels/complete-logo-white-timelapse.png" alt="logo full verison Timelapse introduction">
+			<h1>Montres intemporelles de collection</h1>
+			<a class="removeme" href="#" id="action">Trouvez votre modèle<br/>
+				<i class="fa fa-angle-down" aria-hidden="true"></i>
+			</a>
+		</div>
 
 		<!--<img id="visuel-introduction" src="<?= _URL_IMAGES; ?>/_visuels_dyns/visuel-intro.png" alt="visuel Timelapse introduction">-->
 	</section>
@@ -67,9 +68,9 @@ session_start();
 
 
 
-	<?php do_action( 'foundationpress_after_body' ); ?>
+<?php do_action( 'foundationpress_after_body' ); ?>
 
-	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
 	<div class="off-canvas-wrapper">
 		<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
 			<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
@@ -86,26 +87,37 @@ session_start();
 			</div>
 
 			<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
-				<div class="top-bar-left">
+
+				<div class="top-bar-right"> <!-- SEARCHBAR -->
 					<ul class="menu">
-						<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-					</ul>
-				</div>
-				<div class="top-bar-right">
-					<?php foundationpress_top_bar_r(); ?>
+						<li class="searchbarr">
 
-					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
+							<article id="search-2" class="widget widget_search">
+								<div class="site-recherche">
+									
+									<?php get_search_form( ); ?>
+									<div class="rechercheavance">Recherche avancée</div>
+								</div>
+						</article>
+					</li>
+				</ul>
 			</div>
-		</nav>
-	</header>
 
-	<?php include('custombutton.php'); ?>
+			<div class="top-bar-left"> <!-- NAVIGATION MENU -->
+				<?php foundationpress_top_bar_r(); ?>
 
-	<section class="container">
+				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
+				<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+			<?php endif; ?>
+		</div>
+	</nav>
+</header>
+
+<?php include('custombutton.php'); ?>
+
+<section class="container">
 
 
-		<?php
+	<?php
 				// --> Injection slider my_slider 10
-		do_action( 'foundationpress_after_header' );
+	do_action( 'foundationpress_after_header' );
